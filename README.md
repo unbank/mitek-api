b73df087078dc0c3860683d97b392bb85ab37a39
Oshane Bailey <b4.oshany@gmail.com>
2021-09-27 19:41:21 +0000

    Update the readme and add documentation to the soruce code

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# mitek-api
-PHP Library for Mitek Identity Cloud! An API for authenticating and extracting data from identity documents.
+# PHP Library for Mitek API
+PHP API Library for Mitek Identity Cloud that provides authenticating and extracting data from identity documents. 
+
+This library aims to provice all the API found on the [Mitek API Docs](https://docs.us.mitekcloud.com/#introduction) page.
+
+### What's Mitek
+Mitek is a Digital identity solutions that is designed to prevent fraud, increase conversions and streamline operations
+
+Mitek uses OAuth v2 with JWT tokens for authorization and OpenID for authentication. This token-based standard leverages temporary tokens that provide access to a resource for a limited duration. In our production environments, these tokens will be valid for 5 minutes from the time the token is first issued. When you are requesting one of these temporary tokens, you need to provide the resource you are trying to access (in OAuth this is called the scope). It is best practice to request only the minimally required scope(s) for the operation(s) in order to limit the rights of the access token provided.
+
+for more Mitek see https://docs.us.mitekcloud.com/#introduction 
+
+# Usage
+
+
+## Mobile Verify - Auto
+
+### Code
+
+```php
+<?php
+
+use Unbank\Identity\Mitek\MitekAPI;
+
+
+$mitek = new MitekAPI(
+    null,
+    $request->input('sandbox', null)
+);
+$mitek->auth(
+    $request->input('client_id', config('mitek.client_id')),
+    $request->input('client_secret', config('mitek.client_secret')),
+    $request->input('grant_type', config('mitek.grant_type')),
+    $request->input('scope', config('mitek.scope'))
+);
+$response = $mitek->verify(
+    $request->input('images', array()),
+    $user->getCustomerReferenceId(true),
+    $request->input('type','IdDocument')
+);
+
+echo $response['evidence']["extractedData"]["name"]["fullName"]
+
+?>
+```
+
+For more information about Verify Auto - Request, see
+https://docs.us.mitekcloud.com/#mobile-verify-auto
+
+### Result
+
+```
+OSHANE BAILEY
+```
+
+
+For more information about Verify Auto - Response, see
+https://docs.us.mitekcloud.com/#verify-auto-response
+
+
+# Issue Tracker
+Please use the `issue tracker <http://github.com/unbank/mitek-api/issues>`_
+
+
+# License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
--- src/MitekAPI.php
+++ src/MitekAPI.php
@@ -4,6 +4,14 @@ namespace Unbank\Identity\Mitek;
 
 use Carbon\Carbon;
 
+
+/**
+ * PHP Library for Mitek API
+ *
+ * Mitek uses OAuth v2 with JWT tokens for authorization and OpenID for authentication.
+ * This token-based standard leverages temporary tokens that provide access to a resource for
+ * a limited duration.
+ */
 class MitekAPI
 {
 
@@ -17,13 +25,36 @@ class MitekAPI
     protected $production_api_url = 'https://api.sandbox.west-1.us.mitekcloud.com';
     protected $sandbox_api_url = 'https://api.sandbox.west-1.us.mitekcloud.com';
 
+
+    /**
+     * Construct for the MitekAPI class
+     *
+     * @param string $token         Mitek Auth Token. If no, token is provided, execute the MitekAPI::auth function to generate a new token.
+     * @param boolean $sandbox      Use Mitek Sandbox API URL instead of the Production API URL if set to `true`. Default: `false`
+     */
     public function __construct($token = null, $sandbox=false)
     {
         $this->token = $token;
         $this->api_url = ( $sandbox )? $this->sandbox_api_url : $this->production_api_url;
     }
 
-    public function auth($client_id, $client_secret, $grant_type='client_credentials', $scope='global.identity.api dossier.creator')
+    /**
+     * Get the authenication token for Mitek API.
+     *
+     * @link https://docs.us.mitekcloud.com/#authentication-request
+     *
+     * @param string $client_id         Client ID for Mitek API
+     * @param string $client_secret     Client Secret for Mitek API
+     * @param string $grant_type        OAuth credential type. Default client_credential
+     * @param string $scope             API Scope for Mitek services. See https://docs.us.mitekcloud.com/#api-scopes for more details.
+     * @return array                    An array of the authentication response from Mitek that includes the token to be used in
+     *                                  subsequent requests and the number of seconds the token is valid for.
+     */
+    public function auth(
+        string $client_id,
+        string $client_secret,
+        string $grant_type='client_credentials',
+        string $scope='global.identity.api dossier.creator')
     {
         $this->client_id = $client_id;
         $this->client_secret = $client_secret;
@@ -62,12 +93,26 @@ class MitekAPI
         return $data;
     }
 
+    /**
+     * Convert file to Base64
+     *
+     * @param string $path      Path to the file.
+     * @return string           Base64 data
+     */
     public static function imageToBase64($path) {
         $data = file_get_contents($path);
         return base64_encode($data);
     }
 
-    protected function tokenRequest($url, $postData) {
+    /**
+     * Send API request
+     *
+     * @uses Mitek::auth        Get the authentication token from the response.
+     * @param [type] $url       API Endpoint
+     * @param [type] $postData  HTTP Post Data
+     * @return array            Returns Mitek JSON repsonse as an array.
+     */
+    protected function request($url, $postData) {
         $curl = curl_init();
         curl_setopt_array($curl, array(
           CURLOPT_URL => $url,
@@ -89,6 +134,18 @@ class MitekAPI
         return json_decode($response, true);
     }
 
+    /**
+     * Verify Document
+     *
+     * @link https://docs.us.mitekcloud.com/#verify-auto-response
+     *
+     * @param array $images                     A base64-encoded image containing a single page of the
+     *                                          selfie to be used for face comparison and face liveness
+     * @param string $customer_reference_id     [OPTIONAL] Customer provided identifier that will be
+     *                                          returned in an identically named field within the body of any subsequent responses
+     * @param string $docType                   Type of evidence to be processed.
+     * @return array                            Returns Mitek API Verify Auto - Response as an array.
+     */
     public function verify(array $images, string $customer_reference_id='', string $docType='IdDocument')
     {
 
@@ -110,7 +167,7 @@ class MitekAPI
                 }
             ]
         }';
-        $data = $this->tokenRequest("$this->api_url/api/verify/v2/dossier", $postData);
+        $data = $this->request("$this->api_url/api/verify/v2/dossier", $postData);
         return $data;
     }
 
